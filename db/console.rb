75cb02d59cba3eaaa7032a9de2a07ef6f03e4620
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-06 15:20:21 +0000

    film class created and tested with CRUD

--- db/console.rb
+++ db/console.rb
@@ -3,6 +3,7 @@ require_relative('../models/customer')
 require_relative('../models/film')
 require_relative('../models/ticket')
 
+Film.delete_all()
 Customer.delete_all()
 
 customer1 = Customer.new({
@@ -10,7 +11,37 @@ customer1 = Customer.new({
   'funds' => "100"
   })
 customer1.save()
+customer2 = Customer.new({
+  'name' => "Lynsey",
+  'funds' => "75"
+  })
+customer2.save()
+customer3 = Customer.new({
+  'name' => "Lewis",
+  'funds' => "25"
+  })
+customer3.save()
+customer4 = Customer.new({
+  'name' => "Finlay",
+  'funds' => "30"
+  })
+customer4.save()
 
+film1 = Film.new({
+  'title' => "Blade Runner",
+  'price' => 8
+  })
+film1.save()
+film2 = Film.new({
+  'title' => "Amelie",
+  'price' => 7
+  })
+film2.save()
+film3 = Film.new({
+  'title' => "Chinatown",
+  'price' => 9
+  })
+film3.save()
 
 binding.pry
 nil
---
+++ models/film.rb
@@ -0,0 +1,45 @@
+require_relative('../db/sql_runner')
+require_relative('customer')
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO films (
+      title,
+      price
+    ) VALUES ($1, $2) RETURNING id;"
+    values = [@title, @price]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM films;"
+    films = SqlRunner.run(sql)
+    result = map_films(films)
+  end
+
+  def update()
+    sql = "UPDATE films SET title = $1, price = $2 WHERE id = $3;"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films;"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_films(film_data)
+    film_data.map{|film| Film.new(film)}
+  end
+
+end
